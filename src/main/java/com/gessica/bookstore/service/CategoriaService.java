65a252ab09d6a52360af4ae5e88a37dd8219869a
GessicaP <projetos.gessica@gmail.com>
2022-06-22 11:30:07 -0300

    Update para categoria

--- src/main/java/com/gessica/bookstore/service/CategoriaService.java
+++ src/main/java/com/gessica/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gessica.bookstore.domain.Categoria;
+import com.gessica.bookstore.dtos.CategoriaDTO;
 import com.gessica.bookstore.repositories.CategoriaRepository;
 import com.gessica.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,16 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+		
 	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+	
+	
 
 }
