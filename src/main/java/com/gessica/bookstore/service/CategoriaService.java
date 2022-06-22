26ff51137d19908a2eb254957b2ac410d72a7238
GessicaP <projetos.gessica@gmail.com>
2022-06-22 10:31:47 -0300

    Excecao personalizada para objeto não encontrado

--- src/main/java/com/gessica/bookstore/service/CategoriaService.java
+++ src/main/java/com/gessica/bookstore/service/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.gessica.bookstore.domain.Categoria;
 import com.gessica.bookstore.repositories.CategoriaRepository;
+import com.gessica.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
